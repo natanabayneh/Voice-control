4eb92e38246918a15a6d565f95fc6be1d6e2e4b1
Natan Abayneh <natiabayneh374@gmail.com>
2026-09-03 08:44:00 +0300

    first commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.smoltrade.voice_command_robot"
+    namespace = "com.example.voice_command_robot"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.smoltrade.voice_command_robot"
+        applicationId = "com.example.voice_command_robot"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/smoltrade/voice_command_robot/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.smoltrade.voice_command_robot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
